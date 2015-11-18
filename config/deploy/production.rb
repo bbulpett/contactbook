db8af0b3c57cd0ac15401d214f545582643b2fd5
Barnabas Bulpett <bbulpett@charter.net>
2015-11-18 11:26:00 -0500

    Set up capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-
+server '52.91.51.132', user: 'deploy', roles: %w{web app db}
 
 
 # role-based syntax
